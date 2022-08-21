a5fea84e6d91281e622375fed2a8268b747ece79
Rajat Kumar Dash <rajat.dash@digital.datamatics.com>
2022-08-21 21:43:49 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/newsapp/di/NetworkModule.kt
@@ -0,0 +1,50 @@
+package com.example.newsapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+const val BASE_URL = "https://15.206.209.151/"
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttp(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    @Named("loggingInterceptor")
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+//    @Provides
+//    fun provideApiClient(retrofit: Retrofit): ApiClient {
+//        return retrofit.create(ApiClient::class.java)
+//    }
+
+}
